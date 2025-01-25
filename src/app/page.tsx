72caefd2368124b7dc303cb0c8acead1ff2a8a8b
laibaali85 <laibaali858452@gmail.com>
2025-01-25 07:34:58 -0800

    Day 1 to Day 6 completed

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import FeaturePost from "./components/HomeComponent/featureProduct";
+import Carousel_3 from "./components/HomeComponent/neuralUniverse";
+import Carousel from "./components/HomeComponent/newCollection";
+import ProductCard from "./components/HomeComponent/product";
+import Carousel_2 from "./components/HomeComponent/vitaClassicProduct";
+
+export default async function Home() {
+  return (
+    <div>
+      <Carousel />
+      <ProductCard />
+      <Carousel_2 />
+      <Carousel_3 />
+      <FeaturePost />
+      
+
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        myDark:"#252B42",
+        myGrey:"#737373",
+        myLightGrey:"#BDBDBD",
+        myBlue:"#23A6F0",
+        myGreen:"#2DC071",
+        myDarkGreen:"#23856D",
+        myOrange:"#E77C40",
+        myLightBlue:"#8EC2F2",
+        myBlueImage:"#00b3dd",
+        myDarkOrange:"#E74040",
+        myImageBackground:"#ffe9ea",
+        myImageBackground2: "#967df3",
+        myGreyBackground:"#FAFAFA",
+        mybackgroundblue:"#2A7CC7",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
+""
